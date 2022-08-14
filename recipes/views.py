ff2d0b8c2d3a382bac7b519301770129f344a326
André Amorim <deds5879@gmail.com>
2022-08-14 18:12:33 -0300

    criaçao do app recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse('HOME 2')
+
+
+def contato(request):
+    return HttpResponse('contato 2')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
