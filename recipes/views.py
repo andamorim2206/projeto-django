3d4b70c845d45c7fcad0bb984e937d995fcbb2d9
André Amorim <deds5879@gmail.com>
2022-09-27 09:14:54 -0300

    templates

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otavio',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otavio',
+    })
