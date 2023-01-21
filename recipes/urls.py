f2edad8ff74871c4ed13f93905a5814f093b158c
André Amorim <deds5879@gmail.com>
2023-01-21 11:25:52 -0300

    correçoes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'recipes'
 # HTTP REQUEST
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
